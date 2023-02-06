a4c1aeda091c030287ef6c6666782113cd8f8ce3
Alexis <120563837+alexisart@users.noreply.github.com>
2023-02-05 21:56:23 -0500

    Rate Limit Checking

--- download-bills.py
+++ download-bills.py
@@ -1,6 +1,7 @@
 import os
 import yaml
 import json
+import time
 import boto3
 import requests
 import humanize
@@ -27,11 +28,21 @@ def upload_file(config: dict, key: str, body: str):
 	)
 
 def save_local(key: str, body: str):
-	os.makedirs(os.path.dirname(key))
+	path: str = os.path.dirname(key)
+	if not os.path.exists(path):
+		os.makedirs(path)
 	
 	file = open(key, 'w')
 	file.write(body)
 	file.close()
+
+def get_rate_limit(response):
+	# Monitor Rate Limit - 1,000 Requests Per Hour
+	rate_limit = response.headers["x-ratelimit-limit"]
+	rate_limit_remaining = response.headers["x-ratelimit-remaining"]
+	#print("Rate Limit: %s/%s" % (rate_limit_remaining, rate_limit))
+	
+	return rate_limit, rate_limit_remaining
 
 def get_bills(config: dict):
 	offset: int = 0
@@ -40,9 +51,17 @@ def get_bills(config: dict):
 	
 	while loop:
 		url = "https://api.congress.gov/v3/bill?api_key=%s&offset=%s&limit=%s&format=json" % (config["api_key"], offset, limit)
-		
 		response = requests.get(url=url)
 		results = response.json()
+		
+		rate_limit, rate_limit_remaining = get_rate_limit(response)
+		if response.status_code != 200:
+			print(results["error"]["message"])
+			print("Waiting 10 Minutes To Try Again...")
+			time.sleep(60*10)
+			
+			response = requests.get(url=url)
+			results = response.json()
 		
 		# We are at the end of the results, stop looping
 		if ("next" not in results["pagination"]):
@@ -64,6 +83,13 @@ def get_bills(config: dict):
 			
 			data = get_bill(url=bill["url"], api_key=config["api_key"])
 			
+			if "error" in data:
+				print(data["error"]["message"])
+				print("Waiting 10 Minutes To Try Again...")
+				time.sleep(60*10)
+			
+				data = get_bill(url=bill["url"], api_key=config["api_key"])
+			
 			yield key, data, count, total
 
 		offset += limit
@@ -75,21 +101,27 @@ def get_bill(url: str, api_key: str):
 	return response.json()
 
 def get_bill_actions(url: str, api_key: str):
+	# TODO: Paginate
 	url = "%s&api_key=%s" % (url, api_key)
 	
 def get_bill_committees(url: str, api_key: str):
+	# TODO: Paginate
 	url = "%s&api_key=%s" % (url, api_key)
 
 def get_bill_cosponsors(url: str, api_key: str):
+	# TODO: Paginate
 	url = "%s&api_key=%s" % (url, api_key)
 
 def get_bill_text(url: str, api_key: str):
+	# TODO: Paginate
 	url = "%s&api_key=%s" % (url, api_key)
 
 def get_bill_summaries(url: str, api_key: str):
+	# TODO: Paginate
 	url = "%s&api_key=%s" % (url, api_key)
 
 def get_bill_titles(url: str, api_key: str):
+	# TODO: Paginate
 	url = "%s&api_key=%s" % (url, api_key)
 
 if __name__ == "__main__":
