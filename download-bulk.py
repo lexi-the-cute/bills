13a24ad75e6a63a2ebffd8462b61f5db124ab9aa
Alexis <120563837+alexisart@users.noreply.github.com>
2023-02-06 11:54:42 -0500

    Temporary Resume Solution

--- download-bulk.py
+++ download-bulk.py
@@ -2,6 +2,7 @@ import os
 import json
 import requests
 
+# /bulkdata/BILLSTATUS/108/hr/BILLSTATUS-108hr4693.xml
 base_url: str = "https://www.govinfo.gov/bulkdata/json"
 
 headers: dict = {
@@ -32,7 +33,26 @@ def crawl_bulk_download(url: str):
 	response = requests.get(url=url, headers=headers)
 	results = response.json()
 	
+	skip = True
+	noskipagain = False
 	for section in results["files"]:
+		# Temporary Resume Solution
+		if section["justFileName"] == "BILLSTATUS":
+			skip = False
+		elif section["justFileName"] == "108":
+			skip = False
+		elif section["justFileName"] == "hr":
+			skip = False
+		elif section["justFileName"] == "BILLSTATUS-108hr4693.xml":
+			noskipagain = True
+			skip = False
+		else:
+			if not noskipagain:
+				skip = True
+		
+		if skip:
+			continue
+		
 		if section["folder"] == True:
 			file_path: str = "bulk/%s/%s/data.json" % ("/".join(url.split("/")[5:]), section["justFileName"])
 			save_local(key=file_path, body=json.dumps(section))
