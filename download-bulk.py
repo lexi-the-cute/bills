e4b0741b3dc2911726b4fca51a24ced4527dad96
Alexis <120563837+alexisart@users.noreply.github.com>
2023-02-06 15:01:17 -0500

    Fixed Temporary Resume Bug

--- download-bulk.py
+++ download-bulk.py
@@ -29,28 +29,38 @@ def save_file_local(key: str, body: bytes):
 	file.write(body)
 	file.close()
 
+noskipagain = False
 def crawl_bulk_download(url: str):
+	global noskipagain
+	
 	response = requests.get(url=url, headers=headers)
 	results = response.json()
 	
 	skip = True
-	noskipagain = False
 	for section in results["files"]:
 		# Temporary Resume Solution
+		# TODO: If continuing this script, clean it up
 		if section["justFileName"] == "BILLSTATUS":
+			#print("Step 1")
 			skip = False
 		elif section["justFileName"] == "108":
+			#print("Step 2")
 			skip = False
-		elif section["justFileName"] == "hr":
-			skip = False
-		elif section["justFileName"] == "BILLSTATUS-108hr4693.xml":
+		elif section["justFileName"] == "hconres":
+			#print("Step 3 - %s" % noskipagain)
 			noskipagain = True
 			skip = False
+		#elif section["justFileName"] == "BILLSTATUS-108hr998.xml":
+			#print("Step 4")
+			#noskipagain = True
+			#skip = False
 		else:
 			if not noskipagain:
+				#print("Step 4 - %s/%s" % (skip, noskipagain))
 				skip = True
 		
-		if skip:
+		if skip and not noskipagain:
+			#print("No Skip Again - %s - %s" % (noskipagain, section["justFileName"]))
 			continue
 		
 		if section["folder"] == True:
