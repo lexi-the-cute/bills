81579d44f79d931d68bf83480ce83e5576ccc26f
Alexis <120563837+alexisart@users.noreply.github.com>
2023-02-09 00:01:04 -0500

    Updated Download Script

--- usa/federal/congress/api/download-bills.py
+++ usa/federal/congress/api/download-bills.py
@@ -7,6 +7,8 @@ import random
 import requests
 import humanize
 
+# TODO: A lot of this is just repeat code with different keys and URLs. Try to rewrite this to not duplicate code and to be neater
+
 def load_config():
 	with open('config.yml', 'r') as fi:
 		return yaml.safe_load(fi)
@@ -60,7 +62,7 @@ def get_api_key(config):
 		else:
 			current += 1
 
-def get_bills(config: dict):
+def get_bills():
 	offset: int = 0
 	limit: int = 250
 	loop: bool = True
@@ -98,20 +100,20 @@ def get_bills(config: dict):
 			if (os.path.exists("local/%s" % key)):
 				continue
 			
-			data = get_bill(url=bill["url"], api_key=next(api_key))
+			data = get_json(url=bill["url"], api_key=next(api_key))
 			
 			if "error" in data:
 				print(data["error"]["message"])
 				print("Waiting 60 Minutes To Try Again...")
 				time.sleep(60*60)
 			
-				data = get_bill(url=bill["url"], api_key=next(api_key))
+				data = get_json(url=bill["url"], api_key=next(api_key))
 			
 			yield key, data, count, total
 
 		offset += limit
 	
-def get_bill(url: str, api_key: str):
+def get_json(url: str, api_key: str):
 	url = "%s&api_key=%s" % (url, api_key)
 	
 	response = requests.get(url=url)
@@ -141,25 +143,201 @@ def get_bill_titles(url: str, api_key: str):
 	# TODO: Paginate
 	url = "%s&api_key=%s" % (url, api_key)
 
-if __name__ == "__main__":
+def get_members():
+	offset: int = 0
+	limit: int = 250
+	loop: bool = True
+	
+	global api_key
+	while loop:
+		url = "https://api.congress.gov/v3/member?api_key=%s&offset=%s&limit=%s&format=json" % (next(api_key), offset, limit)
+		response = requests.get(url=url)
+		results = response.json()
+		
+		rate_limit, rate_limit_remaining = get_rate_limit(response)
+		if response.status_code != 200:
+			print(results["error"]["message"])
+			print("Waiting 60 Minutes To Try Again...")
+			time.sleep(60*60)
+			
+			response = requests.get(url=url)
+			results = response.json()
+		
+		# We are at the end of the results, stop looping
+		if ("next" not in results["pagination"]):
+			loop = False
+		
+		total = results["pagination"]["count"]
+		count = offset-1
+
+		for member in results["members"]:
+			count += 1
+			member_id = member["bioguideId"]
+			key = "usa/federal/congress/members/%s/data.json" % member_id
+			
+			# TODO: Make Better Restart Check
+			if (os.path.exists("local/%s" % key)):
+				continue
+			
+			data = get_json(url=member["url"], api_key=next(api_key))
+			
+			if "error" in data:
+				print(data["error"]["message"])
+				print("Waiting 60 Minutes To Try Again...")
+				time.sleep(60*60)
+			
+				data = get_json(url=member["url"], api_key=next(api_key))
+			
+			yield key, data, count, total
+
+		offset += limit
+
+def get_amendments():
+	offset: int = 0
+	limit: int = 250
+	loop: bool = True
+	
+	global api_key
+	while loop:
+		url = "https://api.congress.gov/v3/amendment?api_key=%s&offset=%s&limit=%s&format=json" % (next(api_key), offset, limit)
+		response = requests.get(url=url)
+		results = response.json()
+		
+		rate_limit, rate_limit_remaining = get_rate_limit(response)
+		if response.status_code != 200:
+			print(results["error"]["message"])
+			print("Waiting 60 Minutes To Try Again...")
+			time.sleep(60*60)
+			
+			response = requests.get(url=url)
+			results = response.json()
+		
+		# We are at the end of the results, stop looping
+		if ("next" not in results["pagination"]):
+			loop = False
+		
+		total = results["pagination"]["count"]
+		count = offset-1
+
+		for amendment in results["amendments"]:
+			count += 1
+			session = amendment["congress"]
+			amendment_type = amendment["type"].lower()
+			amendment_number = amendment["number"]
+			key = "usa/federal/congress/amendments/%s/%s/%s/data.json" % (session, amendment_type, amendment_number)
+			
+			# TODO: Make Better Restart Check
+			if (os.path.exists("local/%s" % key)):
+				continue
+			
+			data = get_json(url=amendment["url"], api_key=next(api_key))
+			
+			if "error" in data:
+				print(data["error"]["message"])
+				print("Waiting 60 Minutes To Try Again...")
+				time.sleep(60*60)
+			
+				data = get_json(url=amendment["url"], api_key=next(api_key))
+			
+			yield key, data, count, total
+
+		offset += limit
+
+def get_committee_reports():
+	offset: int = 0
+	limit: int = 250
+	loop: bool = True
+	
+	global api_key
+	while loop:
+		url = "https://api.congress.gov/v3/committee-report?api_key=%s&offset=%s&limit=%s&format=json" % (next(api_key), offset, limit)
+		response = requests.get(url=url)
+		results = response.json()
+		
+		rate_limit, rate_limit_remaining = get_rate_limit(response)
+		if response.status_code != 200:
+			print(results["error"]["message"])
+			print("Waiting 60 Minutes To Try Again...")
+			time.sleep(60*60)
+			
+			response = requests.get(url=url)
+			results = response.json()
+		
+		# We are at the end of the results, stop looping
+		if ("next" not in results["pagination"]):
+			loop = False
+		
+		total = results["pagination"]["count"]
+		count = offset-1
+
+		for committee_report in results["reports"]:
+			count += 1
+			session = committee_report["congress"]
+			committee_report_type = committee_report["type"].lower()
+			committee_report_number = committee_report["number"]
+			# usa/federal/congress/committee-reports/$congress/$committeeReportType/$committeeReportNumber/data.json
+			key = "usa/federal/congress/committee-reports/%s/%s/%s/data.json" % (session, committee_report_type, committee_report_number)
+			
+			# TODO: Make Better Restart Check
+			if (os.path.exists("local/%s" % key)):
+				continue
+			
+			data = get_json(url=committee_report["url"], api_key=next(api_key))
+			
+			if "error" in data:
+				print(data["error"]["message"])
+				print("Waiting 60 Minutes To Try Again...")
+				time.sleep(60*60)
+			
+				data = get_json(url=committee_report["url"], api_key=next(api_key))
+			
+			yield key, data, count, total
+
+		offset += limit
+
+def get_config():
 	config = load_config()
 	s3 = load_s3_client(config['s3'])
 	
 	api_key = get_api_key(config["congress"])
-	#for item in get_api_key(config["congress"]):
-		#print(item)
 	
-	for key, bill, count, total in get_bills(config['congress']):
-		# usa/federal/congress/bills/$congress/$billType/$billNumber/data.json
-		# usa/federal/congress/bills/$congress/$billType/$billNumber/actions/$page.json
-		# usa/federal/congress/bills/$congress/$billType/$billNumber/committees.json
-		# usa/federal/congress/bills/$congress/$billType/$billNumber/cosponsors/$page.json
-		# usa/federal/congress/bills/$congress/$billType/$billNumber/text/$page.json
-		# usa/federal/congress/bills/$congress/$billType/$billNumber/summaries/$page.json
-		# usa/federal/congress/bills/$congress/$billType/$billNumber/titles/$page.json
-		
+	return config, s3, api_key
+
+def download_bills(config):
+	for key, bill, count, total in get_bills():
 		bill_text = json.dumps(bill)
 		
-		print("Uploading (%s, %s): %s" % (humanize.intcomma(count), humanize.intcomma(total), key))
+		print("Uploading Bill (%s, %s): %s" % (humanize.intcomma(count), humanize.intcomma(total), key))
 		save_local(key="local/%s" % key, body=bill_text)
 		upload_file(config=config['s3'], key=key, body=bill_text)
+
+def download_members(config):
+	for key, member, count, total in get_members():
+		member_text = json.dumps(member)
+		
+		print("Uploading Member (%s, %s): %s" % (humanize.intcomma(count), humanize.intcomma(total), key))
+		save_local(key="local/%s" % key, body=member_text)
+		upload_file(config=config['s3'], key=key, body=member_text)
+
+def download_amendments(config):
+	for key, amendment, count, total in get_amendments():
+		amendment_text = json.dumps(amendment)
+		
+		print("Uploading Amendment (%s, %s): %s" % (humanize.intcomma(count), humanize.intcomma(total), key))
+		save_local(key="local/%s" % key, body=amendment_text)
+		upload_file(config=config['s3'], key=key, body=amendment_text)
+
+def download_committee_reports(config):
+	for key, report, count, total in get_committee_reports():
+		report_text = json.dumps(report)
+		
+		print("Uploading Committee Report (%s, %s): %s" % (humanize.intcomma(count), humanize.intcomma(total), key))
+		save_local(key="local/%s" % key, body=report_text)
+		upload_file(config=config['s3'], key=key, body=report_text)
+
+if __name__ == "__main__":
+	config, s3, api_key = get_config()
+	download_bills(config)
+	download_members(config)
+	download_committee_reports(config)
+	download_amendments(config)
