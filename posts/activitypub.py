bb4d59634aa0eff6fd3c73c2ea9064c0176e7e90
Alexis <120563837+alexisart@users.noreply.github.com>
2023-03-04 15:48:01 -0500

    Got 'Accounts' Implemented

--- posts/activitypub.py
+++ posts/activitypub.py
@@ -14,31 +14,6 @@ web_domain = "http://%s" % web_hostname
 
 app = flask.Flask(__name__)
 
-actor: dict = {
-	"@context": [
-		"https://www.w3.org/ns/activitystreams",
-		"https://w3id.org/security/v1"
-	],
-	"id": None,
-	"type": "Person",
-	"preferredUsername": None,
-	"inbox": None,
-	"publicKey": {
-		"id": None,
-		"owner": None,
-		"publicKeyPem": "-----BEGIN PUBLIC KEY-----...-----END PUBLIC KEY-----"
-	}
-}
-
-webfinger: dict = {
-	"subject": None,
-	"links": [{
-		"rel": "self",
-		"type": "application/activity+json",
-		"href": None
-	}]
-}
-
 # TODO: Figure out how to host "existing" posts
 message: dict = {
 	"@context": "https://www.w3.org/ns/activitystreams",
@@ -70,44 +45,150 @@ def get_config():
 	
 	return config, activitypub
 
-@app.route("/actor")
-def path_actor():
+@app.route("/users/<short_id>")
+def path_actor(short_id: str):
+	global ap
+	
+	# For Debugging
+	disable_redirect: bool = False
+	
 	accepted: str = flask.request.headers.get('Accept')
 	
-	if "application/activity+json" in accepted:
+	actor: dict = {
+		"@context": [
+			"https://www.w3.org/ns/activitystreams",
+			"https://w3id.org/security/v1"
+		],
+		"id": None,
+		"type": "Service",
+		"preferredUsername": None,
+		"name": None,
+		"inbox": None,
+		"outbox": None,
+		"following": None,
+		"followers": None,
+		"publicKey": {
+			"id": None,
+			"owner": None,
+			"publicKeyPem": "-----BEGIN PUBLIC KEY-----...-----END PUBLIC KEY-----"
+		}
+	}
+	
+	actor["id"] = "%s/users/%s" % (ap["web_domain"], short_id)
+	actor["preferredUsername"] = "%s" % short_id
+	actor["inbox"] = "%s/users/%s/inbox" % (ap["web_domain"], short_id)
+	actor["outbox"] = "%s/users/%s/outbox" % (ap["web_domain"], short_id)
+	actor["following"] = "%s/users/%s/following" % (ap["web_domain"], short_id)
+	actor["followers"] = "%s/users/%s/followers" % (ap["web_domain"], short_id)
+	actor["publicKey"]["id"] = "%s/users/%s#main-key" % (ap["web_domain"], short_id)
+	actor["publicKey"]["owner"] = "%s/users/%s" % (ap["web_domain"], short_id)
+	actor["publicKey"]["publicKeyPem"] = ap["activitypub_public_key"]
+	
+	actor["name"] = "%s <bot>" % short_id
+	
+	if "application/activity+json" in accepted or disable_redirect:
 		resp = flask.Response(json.dumps(actor))
 		resp.headers['Content-Type'] = 'application/activity+json; charset=utf-8'
 	else:
 		resp = flask.Response("", 302)
-		resp.headers['Location'] = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+		resp.headers['Location'] = "%s/@%s" % (ap["web_domain"], short_id)
 	
 	return resp
 
 @app.route("/.well-known/webfinger")
 def path_webfinger():
+	global ap
+	
+	resource: str = flask.request.args.get('resource', None, type=str)
+	
+	# Missing Resource
+	if resource is None or resource == "":
+		resp = flask.Response("", 400)
+		return resp
+	
+	# Not A Known Resource
+	if not resource.startswith("acct:"):
+		resp = flask.Response("", 400)
+		return resp
+	
+	split = resource.split(":")[1]
+	split = split.split("@")
+	domain = split[1]
+	username = split[0]
+	
+	# Verify At Least 2 Sections
+	if len(split) != 2:
+		resp = flask.Response("", 400)
+		return resp
+	
+	webfinger: dict = {
+		"subject": None,
+		"links": [{
+			"rel": "self",
+			"type": "application/activity+json",
+			"href": None
+		}]
+	}
+	
+	webfinger["subject"] = resource
+	webfinger["links"][0]["href"] = "https://%s/users/%s" % (domain, username)
+	
 	resp = flask.Response(json.dumps(webfinger))
 	resp.headers['Content-Type'] = 'application/jrd+json; charset=utf-8'
+	
+	return resp
+
+@app.route("/users/<short_id>/inbox")
+def path_inbox(short_id: str):
+	resp = flask.Response(json.dumps({}))
+	resp.headers['Content-Type'] = 'application/activity+json; charset=utf-8'
+	
+	return resp
+	
+@app.route("/users/<short_id>/outbox")
+def path_outbox(short_id: str):
+	resp = flask.Response(json.dumps({}))
+	resp.headers['Content-Type'] = 'application/activity+json; charset=utf-8'
+	
+	return resp
+
+@app.route("/users/<short_id>/following")
+def path_following(short_id: str):
+	resp = flask.Response(json.dumps({}))
+	resp.headers['Content-Type'] = 'application/activity+json; charset=utf-8'
+	
+	return resp
+
+@app.route("/users/<short_id>/followers")
+def path_followers(short_id: str):
+	resp = flask.Response(json.dumps({}))
+	resp.headers['Content-Type'] = 'application/activity+json; charset=utf-8'
+	
+	return resp
+
+@app.route("/@<short_id>")
+def path_actor_html(short_id: str):
+	html: str = """
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<meta charset="UTF-8"> 
+			<title>User: %s</title>
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		</head>
+		<body>
+			<h1>User: %s</h1>
+		</body>
+		</html>
+	""" % (short_id, short_id)
+	
+	resp = flask.Response(html)
+	resp.headers['Content-Type'] = 'text/html; charset=utf-8'
 	
 	return resp
 
 if __name__ == "__main__":
 	config, ap = get_config()
 	
-	hostName = ap["hostname"]
-	serverPort = ap["server_port"]
-
-	account_name = ap["account_name"]
-	web_hostname = ap["web_hostname"]
-	web_domain = ap["web_domain"]
-	
-	actor["id"] = "%s/actor" % web_domain
-	actor["preferredUsername"] = "%s" % account_name
-	actor["inbox"] = "%s/inbox" % web_domain
-	actor["publicKey"]["id"] = "%s/actor#main-key" % web_domain
-	actor["publicKey"]["owner"] = "%s/actor" % web_domain
-	actor["publicKey"]["publicKeyPem"] = ap["activitypub_public_key"]
-	webfinger["subject"] = "acct:%s@%s" % (account_name, web_hostname)
-	webfinger["links"][0]["href"] = "%s/actor" % web_domain
-	
 	#app.run(debug=False, ssl_context=(ap["certificate_path"], ap["private_key_path"]), host=hostName, port=serverPort)
-	app.run(debug=False, host=hostName, port=serverPort)
+	app.run(debug=True, host=ap["hostname"], port=ap["server_port"])
