d241ee41be99555a148241dff646ba9710ce018a
Alexis <120563837+alexisart@users.noreply.github.com>
2023-03-04 01:59:23 -0500

    Updating ActivityPub Test

--- posts/activitypub.py
+++ posts/activitypub.py
@@ -9,57 +9,33 @@ hostName = "localhost"
 serverPort = 8080
 
 account_name = "demo"
-web_hostname = "localhost"
+web_hostname = "example.com"
 web_domain = "http://%s" % web_hostname
 
 app = flask.Flask(__name__)
-
-def load_config():
-	with open('config.yml', 'r') as fi:
-		return yaml.safe_load(fi)
-
-def get_config():
-	config = load_config()
-	activitypub = config['activitypub']
-	
-	return config, activitypub
-
-if __name__ == "__main__":
-	config, ap = get_config()
-	hostName = ap["hostname"]
-	serverPort = ap["server_port"]
-
-	account_name = ap["account_name"]
-	web_hostname = ap["web_hostname"]
-	web_domain = ap["web_domain"]
-	
-	actor["publicKey"]["publicKeyPem"] = ap["activitypub_public_key"]
-	
-	#app.run(debug=False, ssl_context=(ap["certificate_path"], ap["private_key_path"]), host=hostName, port=serverPort)
-	app.run(debug=False, host=hostName, port=serverPort)
 
 actor: dict = {
 	"@context": [
 		"https://www.w3.org/ns/activitystreams",
 		"https://w3id.org/security/v1"
 	],
-	"id": "%s/actor" % web_domain,
+	"id": None,
 	"type": "Person",
-	"preferredUsername": "%s" % account_name,
-	"inbox": "%s/inbox" % web_domain,
+	"preferredUsername": None,
+	"inbox": None,
 	"publicKey": {
-		"id": "%s/actor#main-key" % web_domain,
-		"owner": "%s/actor" % web_domain,
+		"id": None,
+		"owner": None,
 		"publicKeyPem": "-----BEGIN PUBLIC KEY-----...-----END PUBLIC KEY-----"
 	}
 }
 
 webfinger: dict = {
-	"subject": "acct:%s@%s" % (account_name, web_hostname),
+	"subject": None,
 	"links": [{
 		"rel": "self",
 		"type": "application/activity+json",
-		"href": "%s/actor" % web_domain
+		"href": None
 	}]
 }
 
@@ -84,6 +60,16 @@ headers: dict = {
 	"Signature": 'keyId="%s/actor#main-key",headers="(request-target) host date",signature="..."' % web_domain
 }
 
+def load_config():
+	with open('config.yml', 'r') as fi:
+		return yaml.safe_load(fi)
+
+def get_config():
+	config = load_config()
+	activitypub = config['activitypub']
+	
+	return config, activitypub
+
 @app.route("/actor")
 def path_actor():
 	resp = flask.Response(json.dumps(actor))
@@ -97,3 +83,25 @@ def path_webfinger():
 	resp.headers['Content-Type'] = 'application/jrd+json; charset=utf-8'
 	
 	return resp
+
+if __name__ == "__main__":
+	config, ap = get_config()
+	
+	hostName = ap["hostname"]
+	serverPort = ap["server_port"]
+
+	account_name = ap["account_name"]
+	web_hostname = ap["web_hostname"]
+	web_domain = ap["web_domain"]
+	
+	actor["id"] = "%s/actor" % web_domain
+	actor["preferredUsername"] = "%s" % account_name
+	actor["inbox"] = "%s/inbox" % web_domain
+	actor["publicKey"]["id"] = "%s/actor#main-key" % web_domain
+	actor["publicKey"]["owner"] = "%s/actor" % web_domain
+	actor["publicKey"]["publicKeyPem"] = ap["activitypub_public_key"]
+	webfinger["subject"] = "acct:%s@%s" % (account_name, web_hostname)
+	webfinger["links"]["href"] = "%s/actor" % web_domain
+	
+	#app.run(debug=False, ssl_context=(ap["certificate_path"], ap["private_key_path"]), host=hostName, port=serverPort)
+	app.run(debug=False, host=hostName, port=serverPort)
